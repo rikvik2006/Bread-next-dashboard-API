7623a7897994aae5dcd2e2b520a62463d0a42647
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-11 17:52:06 +0200

    removed from stage dist folder

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,37 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const express_1 = __importDefault(require("express"));
+const routers_1 = __importDefault(require("./routers"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3001;
+function createApp() {
+    const app = (0, express_1.default)();
+    app.use("/api", routers_1.default);
+    return app;
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const app = createApp();
+            app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
+main();
