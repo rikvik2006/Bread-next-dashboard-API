9cbb1c63f49df47c2e92640fabfeb92a240635c1
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-15 00:43:10 +0200

    Fetched all bot guild and seted up PartialsGuild so now intellisense can help me (same commit beccause before i don't stage file

--- src/routers/guilds/index.ts
+++ src/routers/guilds/index.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import { getGuildsController } from "../../controllers/guilds";
 import { isAuthenticated } from "../../utils/middlewares";
 const router = Router();
 
-router.get("/", isAuthenticated, (req, res) => {
-    res.send(200);
-})
+router.get("/", isAuthenticated, getGuildsController);
 
 export default router; 
