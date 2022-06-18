7147665757e78d50f8e87e79a693443d8120fdf2
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-18 15:07:24 +0200

    now getGuildsController ser a response of all guilds obj and not element guilds of obj guilds. This caused an error

--- src/controllers/guilds/index.ts
+++ src/controllers/guilds/index.ts
@@ -6,7 +6,7 @@ export async function getGuildsController(req: Request, res: Response) {
     const user = req.user as User;
     try {
         const guilds = await getMutualGuildsService(user.id);
-        res.send({ guilds });
+        res.send(guilds);
     } catch (err) {
         console.log(err)
         res.status(400).send("Error");
